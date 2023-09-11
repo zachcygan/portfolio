20288214bfae0d43e884300c72f877bd6e96a24b
zachcygan <zachcygan@gmail.com>
2023-09-11 00:12:09 -0700

    changed metadata

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,11 @@
 'use client'
+import { Metadata } from 'next' 
 import PortfolioInfo from "../../components/portfolio"
+
+export const metadata: Metadata = {
+    title: 'Zach Cygan - Portfolio',
+    description: 'Zach Cygan\'s portfolio',
+}
 
 export default function Portfolio() {
     return (
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,11 @@
 'use client'
+import { Metadata } from 'next'
 import ResumeInfo from '../../components/resume'
+
+export const metadata: Metadata = {
+  title: 'Zach Cygan - Resume',
+  description: 'Zach Cygan\'s Resume',
+}
 
 export default function Resume() {
   return (
