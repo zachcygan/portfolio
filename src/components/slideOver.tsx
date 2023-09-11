3d77e10711c5dddf0d19dc26aa01ce43c15992cb
zachcygan <zachcygan@gmail.com>
2023-09-10 23:59:39 -0700

    more dark mode

--- src/components/slideOver.tsx
+++ src/components/slideOver.tsx
@@ -59,7 +59,7 @@ export default function SlideOver({ isOpen, onClose }: SlideOverProps) {
                                                 <div className="ml-3 flex h-7 items-center">
                                                     <button
                                                         type="button"
-                                                        className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                                        className="relative rounded-md bg-darkBg text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue focus:ring-offset-2"
                                                         onClick={() => handleClose()}
                                                     >
                                                         <span className="absolute -inset-2.5" />
